bf1f166efcb8f324e36071bd7b5f22dc5ea34ef6
Robert Cinciuc <robertcinciuc@gmail.com>
2025-03-18 21:22:12 +0100

    Added solution to TwoSum V3 - all tests pass

--- src/robertcinciuc/problems/leetcode/array/TwoSum.java
+++ src/robertcinciuc/problems/leetcode/array/TwoSum.java
@@ -7,10 +7,6 @@ public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> valuePosition = new HashMap<>();
-        for(int i = 0; i < nums.length; ++i){
-            valuePosition.put(nums[i], i);
-        }
-
         int[] resp = new int[2];
         for(int i = 0; i < nums.length; ++i){
             if(valuePosition.containsKey(target - nums[i]) && valuePosition.get(target - nums[i]) != i){
@@ -18,6 +14,7 @@ public class TwoSum {
                 resp[1] = valuePosition.get(target - nums[i]);
                 return resp;
             }
+            valuePosition.put(nums[i], i);
         }
 
         return resp;
