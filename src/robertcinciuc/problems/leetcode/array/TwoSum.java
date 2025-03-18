2b450d2e99dea874d052762221ca75e3ba60e552
Robert Cinciuc <robertcinciuc@gmail.com>
2025-03-18 21:16:36 +0100

    Added solution to TwoSum V2 - all tests pass

--- src/robertcinciuc/problems/leetcode/array/TwoSum.java
+++ src/robertcinciuc/problems/leetcode/array/TwoSum.java
@@ -1,15 +1,22 @@
 package robertcinciuc.problems.leetcode.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> valuePosition = new HashMap<>();
+        for(int i = 0; i < nums.length; ++i){
+            valuePosition.put(nums[i], i);
+        }
+
         int[] resp = new int[2];
         for(int i = 0; i < nums.length; ++i){
-            resp[0] = i;
-            for(int j = i + 1; j < nums.length; ++j){
-                if(nums[i] + nums[j] == target){
-                    resp[1] = j;
-                    return resp;
-                }
+            if(valuePosition.containsKey(target - nums[i]) && valuePosition.get(target - nums[i]) != i){
+                resp[0] = i;
+                resp[1] = valuePosition.get(target - nums[i]);
+                return resp;
             }
         }
 
@@ -19,6 +26,6 @@ public class TwoSum {
     public static void main(String[] args){
         TwoSum twoSum = new TwoSum();
         int[] ints = twoSum.twoSum(new int[]{2, 7, 11, 15}, 9);
-        System.out.println(ints);
+        System.out.println(ints[0] + " " + ints[1]);
     }
 }
